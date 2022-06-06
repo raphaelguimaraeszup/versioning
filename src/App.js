dd7a6d3e531d24fee2496e9c0988632416697007
Raphael Guimarães <raphael.guimaraes@zup.com.br>
2022-06-06 17:10:28 -0300

    feat: adiciona step
    BREAKING CHANGE: adiciona step no action

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Testing Versioning
+          Testing Versioning Major
         </a>
       </header>
     </div>
